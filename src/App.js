adc007e3687195ba8d2794286e880c4eb141c5fa
lam-vk <lam-vk@genkisystem.com>
2020-08-31 15:34:10 +0700

    sadsadsad

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.bc
+          Edit <code>src/App.js</code> and save to reload.bcdsadsadsa
+          sascacsalsa
         </p>
         <a
           className="App-link"
